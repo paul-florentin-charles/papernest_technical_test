a2d593c052632938698aff38b945ac10369d54c2
polochinoc <paul.charles@tutanota.com>
2025-05-22 00:16:38 +0200

    Move "utils" variables/functions to utils.py

--- main.py
+++ main.py
@@ -1,80 +1,15 @@
-import json
-import math
-import os
-from collections import defaultdict
 from typing import Any
 
-import pandas as pd
 import requests
 from fastapi import FastAPI, HTTPException, Query
 from haversine import haversine
 
-from utils import lambert93_to_wsg84
-
-# Location of CSV file mapping operator and coordinates to network coverage
-CSV_PATH = os.path.join(
-    "resources", "2018_01_Sites_mobiles_2G_3G_4G_France_metropolitaine_L93.csv"
+from utils import (
+    MAX_ALLOWED_DISTANCE_KM,
+    OPERATOR_NAME_BY_CODE,
+    SATISFACTORY_DISTANCE_KM,
+    load_operator_coverage_cache,
 )
-
-# Operator name by its code (MCC + MNC)
-# Source: https://fr.wikipedia.org/wiki/Mobile_Network_Code#Tableau_des_MNC_pour_la_France_m%C3%A9tropolitaine
-OPERATOR_NAME_BY_CODE: dict[int, str] = {
-    20801: "Orange",
-    20810: "SFR",
-    20815: "Free",
-    20820: "Bouygues",
-}
-ALL_KNOWN_OPERATOR: set[str] = set(OPERATOR_NAME_BY_CODE.values())
-
-# Coverage area limits
-MAX_ALLOWED_DISTANCE_KM = 20.0  # over this distance, we ignore coverage
-SATISFACTORY_DISTANCE_KM = 5.0  # "big" city radius, coverage is good enough
-
-# --- CSV cache for operator code to list of coverage dicts ---
-CACHE_FILE_PATH = os.path.join("cache", "operator_coverage_cache.json")
-
-
-def load_operator_coverage_cache():
-    # Try loading existing cache file first
-    if os.path.exists(CACHE_FILE_PATH):
-        with open(CACHE_FILE_PATH, "r", encoding="utf-8") as f:
-            return json.load(f)
-
-    data_to_cache: dict[str, list[dict]] = defaultdict(list)
-    for row in pd.read_csv(CSV_PATH, dtype=str, sep=";", encoding="utf-8").to_dict(
-        orient="records"
-    ):
-        x, y = row["x"], row["y"]
-        try:
-            x = float(x)
-            y = float(y)
-            if math.isnan(x) or math.isnan(y):
-                continue
-        except (ValueError, TypeError):
-            continue
-
-        lon, lat = lambert93_to_wsg84(x, y)
-        coverage = {
-            "2G": bool(int(row["2G"])),
-            "3G": bool(int(row["3G"])),
-            "4G": bool(int(row["4G"])),
-        }
-
-        data_to_cache[row["Operateur"]].append(
-            {
-                "csv_coords_lambert93": {"lon": x, "lat": y},
-                "csv_coords_gps": {"lon": lon, "lat": lat},
-                "coverage": coverage,
-            }
-        )
-
-    # Save cache as JSON file while creating directory if not existing
-    os.makedirs(os.path.dirname(CACHE_FILE_PATH), exist_ok=True)
-    with open(CACHE_FILE_PATH, "w", encoding="utf-8") as f:
-        json.dump(data_to_cache, f)
-
-    return data_to_cache
-
 
 app = FastAPI(
     title="Mobile Network Coverage API",
